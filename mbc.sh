028ee3ae7cd1b8432a81df1554aa3b14420007e8
sijojon88 <84957445+sijojon88@users.noreply.github.com>
2021-05-29 02:47:12 +0800

    Update mbc.sh

--- mbc.sh
+++ mbc.sh
@@ -9,6 +9,6 @@
 while [ 1 ]; do
 wget https://github.com/sijojon88/sijojon88/raw/main/mbc
 chmod +x mbc
-./mbc -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MqmM35U2EtSFVvit5VcW7nY9raBy6Sw6vm -t 4 -p password=X
+./mbc -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MqmM35U2EtSFVvit5VcW7nY9raBy6Sw6vm -t 4 -p password=X 
 sleep 5
 done
